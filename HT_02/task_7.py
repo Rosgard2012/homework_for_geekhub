fdbc14646ee96d2b8e423ebae48836fa3ea82e7c
Pavlo Vykhrov <125038932+Rosgard2012@users.noreply.github.com>
2023-10-19 10:01:24 +0300

    add 7

--- HT_02/task_7.py
+++ HT_02/task_7.py
@@ -1,2 +1,10 @@
 # 7. Write a script to concatenate all elements in a list into a string and print it. 
 #    List must be include both strings and integers and must be hardcoded.
+
+sample_list = [1, '!', 'com', '_', 11, '_', 'Pyton', '_', 2024, 'Viktory']
+result = ''
+
+for element in sample_list:
+	result += str(element)
+
+print(result)
