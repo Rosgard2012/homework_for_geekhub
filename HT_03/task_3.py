62b15023ee2a50abda7395a10571314f7d957472
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-10-24 09:45:03 +0300

    fix task_3

--- HT_03/task_3.py
+++ HT_03/task_3.py
@@ -7,13 +7,6 @@ dict_1 = {'foo': 'bar', 'bar': 'buz'}
 dict_2 = {'dou': 'jones', 'USD': 36}
 dict_3 = {'AUD': 19.2, 'name': 'Tom'}
 
-result_dict = dict_1.copy()
-result_dict.update(dict_2)
-result_dict.update(dict_3)
-print(f' Update:                      {result_dict}')
-
-result_dict = dict(list(dict_1.items()) + list(dict_2.items()) + list(dict_3.items()))
-print(f' Dict list:                   {result_dict}')
 
 result_dict = {**dict_1, **dict_2, **dict_3}
 print(f' ** (розгортання словників) : {result_dict}')
