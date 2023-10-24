6cb45a1850890ac6b5b724bb7d616a413ea8b078
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-10-24 14:52:23 +0300

    6 - помилка в процесі

--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -1,10 +1,27 @@
 '''6. Write a script to get the maximum and minimum value in a dictionary.'''
 
-dict = {'a': 9, 'b': 5, 'c': 3, 'd': 25, 'e': 115}
+my_dict = {'a': 9, 'b': 'Vasyl', 'c': [3, 7, 4], 'd': {'x': 10, 'y': 20,'z': 30}, 'e': 115}
 
-max_value = max(dict.values())
-min_value = min(dict.values())
+def new_key(value):
+    if isinstance(value, str):
+        return (0, value)
+    if isinstance(value, (list, dict)):
+        return len(str(value))
+    return value
+
+
+max_value = max(my_dict.values(), key=new_key)
+min_value = min(my_dict.values(), key=new_key)
 
 
 print(f"Max value:    {max_value}")
 print(f"Min value:    {min_value}")
+
+
+'''max_k, max_v = max(d.items(), key=lambda x: x[1])
+print(max_k)
+# a
+
+print(max_v)
+# 100
+https://note.nkmk.me/en/python-dict-value-max-min/'''
