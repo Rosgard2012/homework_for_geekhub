c4e5825d31a2f96ac32e716b5e195c9b9fbfc6f6
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-10-26 08:21:16 +0300

    test 4

--- HT_04/task_4.py
+++ HT_04/task_4.py
@@ -1,1 +1,23 @@
-task_2.py
+'''Write a Python program that demonstrates exception chaining. Create a custom exception class called
+CustomError and another called SpecificError. In your program (could contain any logic you want), raise a SpecificError,
+ and then catch it in a try/except block, re-raise it as a CustomError with the original exception as the cause.
+  Display both the custom error message and the original exception message.'''
+
+
+class CustomError(Exception):
+    pass
+
+class SpecificError(Exception):
+    pass
+
+try:
+
+    raise SpecificError("This is a specific error.")
+except SpecificError as specific_exception:
+    try:
+
+        raise CustomError("This is custom error.") from specific_exception
+    except CustomError as custom_exception:
+
+        print("Custom Error message:  ", custom_exception)
+        print("Original Exception Message:  ", custom_exception.__cause__)
