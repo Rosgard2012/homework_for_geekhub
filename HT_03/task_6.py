8175e0eba867b1d93c80d295892cb3bf0741e8d1
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-10-26 07:32:09 +0300

    fix task_6

--- HT_03/task_6.py
+++ HT_03/task_6.py
@@ -1,6 +1,6 @@
 '''6. Write a script to get the maximum and minimum value in a dictionary.'''
 
-my_dict = {'a': 9, 'b': 'Vasyl', 'c': [3, 7, 4], 'e': 115, 'z': 1, 'w': 0, 't': 333}
+my_dict = {'a': 9, 'b': 'Vasyl', 'c': [3, 7, 4], 'e': 115, 'z': 1.11, 'w': 0, 't': 333, 's': True }
 
 
 values = list(filter(lambda value: type(value) in (int, float), my_dict.values()))
@@ -13,12 +13,4 @@ print(f"Max value:    {max_value}")
 print(f"Min value:    {min_value}")
 
 
-'''max_k, max_v = max(d.items(), key=lambda x: x[1])
-print(max_k)
-# a
 
-print(max_v)
-# 100
-https://note.nkmk.me/en/python-dict-value-max-min/
-https://realpython.com/python-min-and-max/
-'''
