fa74a204d358cc59e771c2ae2da719bf548f1190
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-10-26 08:02:10 +0300

    HT-04-task_02

---
+++ HT_04/task_2.py
@@ -0,0 +1,20 @@
+"""Create a custom exception class called NegativeValueError.
+Write a Python program that takes an integer as input and raises the NegativeValueError if the input is negative.
+Handle this custom exception with a try/except block and display an error message."""
+
+class NegativeValueError(Exception):
+    def __init__(self, value):
+        self.value = value
+        super().__init__()
+
+
+try:
+    num = int(input("Enter number: "))
+    if num < 0:
+        raise NegativeValueError(num)
+    else:
+        print("You entered positive number:", num)
+except NegativeValueError as e:
+    print("Error 320115: You entered negative value:", e.value)
+except ValueError:
+    print("Error 331111: Invalid input. Please enter a valid integer. Try again")
