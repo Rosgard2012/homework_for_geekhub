de4f79e895b94fc1e4c937b70b7789f0409b456a
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-11-02 08:44:55 +0200

    fix tasks_01 PEP8

--- HT_06/task_01.py
+++ HT_06/task_01.py
@@ -2,17 +2,15 @@
  квадрата, і вертатиме 3 значення у вигляді кортежа: периметр квадрата,
   площа квадрата та його діагональ."""
 
+
 def square(side):
     perimeter = 4 * side
     area = side ** 2
     diagonal = round(side * (2 ** 0.5), 2)
-    return (perimeter, area, diagonal)
+    return perimeter, area, diagonal
+
 
 side_length = float(input("Введіть довжину сторони квадрата: "))
 
 result = square(side_length)
 print(f"Периметр: {result[0]}, Площа: {result[1]}, Діагональ: {result[2]}")
-
-
-
-
