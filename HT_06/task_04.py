30ead4643d925a1292cca89ba1377dc3c16f68c2
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-11-02 08:04:12 +0200

    HT_06 tasks_04

--- HT_06/task_04.py
+++ HT_06/task_04.py
@@ -3,3 +3,34 @@
  цього діапазона. Не забудьте про перевірку на валідність
  введених даних та у випадку невідповідності - виведіть повідомлення.
 """
+
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number <= 3:
+        return True
+    if number % 2 == 0 or number % 3 == 0:
+        return False
+    for i in range(5, int(number**0.5) + 1, 6):
+        if number % i == 0 or number % (i + 2) == 0:
+            return False
+    return True
+
+
+def prime_list(start, end):
+    if start < 0 or end < 0 or start > end:
+        print("Помилка: невірний діапазон")
+        return []
+
+    primes = [number for number in range(start, end + 1) if is_prime(number)]
+    return primes
+
+
+start_range = int(input("Введіть початок діапазону: "))
+end_range = int(input("Введіть кінець діапазону: "))
+
+prime_numbers = prime_list(start_range, end_range)
+
+if prime_numbers:
+    print(f"Прості числа у діапазоні від {start_range} до {end_range}: {prime_numbers}")
