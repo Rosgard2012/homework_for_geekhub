c085ce3530122d984b69b23cf477bf52faf41a7f
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-11-02 08:15:39 +0200

    HT_06 tasks_06

--- HT_06/task_05.py
+++ HT_06/task_05.py
@@ -1,2 +1,16 @@
 """5. Написати функцію <fibonacci>, яка приймає один аргумент
  і виводить всі числа Фібоначчі, що не перевищують його."""
+
+
+def fibonacci(number):
+    x, y = 0, 1
+    while x <= number:
+        print(x)
+        x, y = y, x + y
+
+
+number = int(input("Введіть число: "))
+
+print("Числа Фібоначчі, що не перевищують", number, ":")
+fibonacci(number)
+
--- HT_06/task_06.py
+++ HT_06/task_06.py
@@ -6,3 +6,29 @@
    Наприклад:
    fnc([1, 2, 3, 4, 5], shift=1) --> [5, 1, 2, 3, 4]
    fnc([1, 2, 3, 4, 5], shift=-2) --> [3, 4, 5, 1, 2]"""
+
+
+def cyclic_shift(lst, shift):
+    if not lst:
+        return lst
+    shift = shift % len(lst)
+    if shift == 0:
+        return lst
+
+    if shift > 0:
+        return lst[-shift:] + lst[:-shift]
+    else:
+        return lst[-shift:] + lst[:-shift]
+
+
+lst1 = [1, 2, 3, 4, 5]
+shift1 = 1
+shift1 = 3
+result1 = cyclic_shift(lst1, shift1)
+print(result1)
+
+lst2 = [1, 2, 3, 4, 5]
+shift2 = -2
+shift2 = -3
+result2 = cyclic_shift(lst2, shift2)
+print(result2)
