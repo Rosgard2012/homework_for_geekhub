70efd8ab2c63681d7a4011a1406be80d5f889072
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-11-02 08:28:45 +0200

    tasks_07

--- HT_06/task_07.py
+++ HT_06/task_07.py
@@ -5,3 +5,23 @@
           1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]
     ----> "1 -> 3, foo -> 2, [1, 2] -> 2, True -> 1"
 """
+
+
+def count_elements(lst):
+    count_temp = {}
+    for element in lst:
+        key = element if not isinstance(element, list) else str(element)
+        if key in count_temp:
+            count_temp[key] += 1
+        else:
+            count_temp[key] = 1
+
+    result = ", ".join(f"{key} -> {value}" for key, value in count_temp.items())
+    print(result)
+
+
+input_list = [1, 1, 'foo', [1, 2], True, 'foo', 1, [1, 2]]
+count_elements(input_list)
+
+input_list2 = [1, 'apple', 'banana', 1, 'apple', 'cherry', 2, 2, 'banana', 'cherry', 'apple', 1]
+count_elements(input_list2)
