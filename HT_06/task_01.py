351a0e77cb260a3f0585a764e35f13f3d09c253c
Rosgard2012 <vihrow.pavlo@gmail.com>
2023-11-02 07:37:39 +0200

    HT_06 tasks_01

--- HT_06/task_01.py
+++ HT_06/task_01.py
@@ -2,7 +2,15 @@
  квадрата, і вертатиме 3 значення у вигляді кортежа: периметр квадрата,
   площа квадрата та його діагональ."""
 
+def square(side):
+    perimeter = 4 * side
+    area = side ** 2
+    diagonal = round(side * (2 ** 0.5), 2)
+    return perimeter, area, diagonal
 
+side_length = input('Введіть сторону квадрату: ')
+perimeter, area, diagonal = square(side_length)
+print(f"Периметр: {perimeter}, Площа: {area}, Діагональ: {diagonal}")
 
 
 
