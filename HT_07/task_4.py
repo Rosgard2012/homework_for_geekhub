64e9b7ee9851e7ad8ea2ab8e5cf97e8f132ccd94
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-11-07 01:06:32 +0200

    test HT_07 task_4

--- HT_07/task_4.py
+++ HT_07/task_4.py
@@ -13,3 +13,50 @@
     --. . . -.- .... ..- -...   .. ...   .... . .-. .
 
     '''
+
+
+def morse_code(input_string):
+    morse_dict = {
+        ".-": "A",
+        "-...": "B",
+        "-.-.": "C",
+        "-..": "D",
+        ".": "E",
+        "..-.": "F",
+        "--.": "G",
+        "....": "H",
+        "..": "I",
+        ".---": "J",
+        "-.-": "K",
+        ".-..": "L",
+        "--": "M",
+        "-.": "N",
+        "---": "O",
+        ".--.": "P",
+        "--.-": "Q",
+        ".-.": "R",
+        "...": "S",
+        "-": "T",
+        "..-": "U",
+        "...-": "V",
+        ".--": "W",
+        "-..-": "X",
+        "-.--": "Y",
+        "--..": "Z",
+        "...---...": "SOS"
+    }
+
+    words = input_string.split('   ')
+    decoded_message = ''
+
+    for word in words:
+        letters = word.split(' ')
+        for letter in letters:
+            decoded_message += morse_dict.get(letter, '')
+        decoded_message += ' '
+
+    return decoded_message
+
+
+print(morse_code('--. . . -.- .... ..- -...   .. ...   .... . .-. .'))
+print(morse_code('...---...'))
