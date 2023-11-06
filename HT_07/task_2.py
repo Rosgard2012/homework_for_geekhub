575d2c3a0630a3780288a72975ad78fd9bbe2db9
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-11-07 00:37:48 +0200

    HT_07 task_2

--- HT_07/task_2.py
+++ HT_07/task_2.py
@@ -5,3 +5,39 @@
    - якесь власне додаткове правило :)
    Якщо якийсь із параментів не відповідає вимогам - породити виключення із
    відповідним текстом.'''
+
+
+def validate_username(username):
+    if len(username) < 3 or len(username) > 50:
+        raise ValueError("Ім'я повинно бути не меншим за 3 символа "
+                         "і не більшим за 50")
+
+
+def validate_password(password):
+    if len(password) < 8 or not any(char.isdigit() for char in password):
+        raise ValueError("Пароль повинен бути не меншим "
+                         "за 8 символів і містити хоча б одну цифру")
+
+
+def additional_rule(username, password):
+    if (username.lower() in password.lower() or password.lower()
+            in username.lower()):
+        raise ValueError("Пароль не повинен містити "
+                         "або повторювати ім'я користувача")
+
+
+def validate_credentials(username, password):
+    validate_username(username)
+    validate_password(password)
+    additional_rule(username, password)
+
+
+try:
+    username_input = input("Введіть ім'я користувача: ")
+    password_input = input("Введіть пароль: ")
+
+    validate_credentials(username_input, password_input)
+    print("Пара ім'я/пароль успішно валідована")
+
+except ValueError as e:
+    print("Помилка валідації:", e)
