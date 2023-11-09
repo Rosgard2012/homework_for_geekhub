0d23296f8c4b12f9f54a0d618317f643b6b066b3
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-11-09 07:51:00 +0200

    HT_08 tasks_05

--- HT_08/task_05.py
+++ HT_08/task_05.py
@@ -12,3 +12,24 @@
     "aA11" -> 2             # 'a' і '1'
     "ABBA" -> 2             # 'A' і 'B' кожна двічі
 '''
+
+
+def count_repeated_chars(input_string):
+    normalized_string = input_string.lower()
+    char_count = {}
+
+    for char in normalized_string:
+        if char.isalnum():
+            char_count[char] = char_count.get(char, 0) + 1
+
+    repeated_chars = [char for char, count in char_count.items() if count > 1]
+    return len(repeated_chars)
+
+
+print(count_repeated_chars("abcde"))
+print(count_repeated_chars("aabbcde"))
+print(count_repeated_chars("aabBcde"))
+print(count_repeated_chars("indivisibility"))
+print(count_repeated_chars("^&TFGCQYUTjhg12vasuydmbasjxysdga"))
+print(count_repeated_chars("aA11"))
+print(count_repeated_chars("ABBA"))
