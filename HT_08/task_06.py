aa36275304914d94c91dc55d856686aa643c5fbb
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-11-09 08:00:33 +0200

    HT_08 tasks_06

--- HT_08/task_06.py
+++ HT_08/task_06.py
@@ -1,3 +1,12 @@
 '''6. Напишіть функцію,яка прймає рядок з декількох слів і повертає
 довжину найкоротшого слова. Реалізуйте обчислення за допомогою генератора.
 '''
+
+
+def shortest_word_length(sentence):
+    word_lengths = (len(word) for word in sentence.split())
+    return min(word_lengths, default=0)
+
+
+text = ('She is kind-hearted and always willing to help those in need , ')
+print(shortest_word_length(text))
