2f7b5142bd850075b29d50a64fda0dd2e0970122
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-11-09 07:28:41 +0200

    HT_08 tasks_03

--- HT_08/task_03.py
+++ HT_08/task_03.py
@@ -12,3 +12,22 @@
     по ній: https://docs.python.org/3/library/stdtypes.html#range
    P.P.P.S Не забудьте обробляти невалідні ситуації (аналог range(1, -10, 5)).
     Подивіться як веде себе стандартний range в таких випадках.'''
+
+
+def my_range(start, stop, step=1):
+    if step == 0:
+        raise ValueError("Step cannot be zero")
+
+    if step > 0 and start >= stop:
+        raise ValueError("arguments error: start >= stop for positive step")
+    elif step < 0 and start <= stop:
+        raise ValueError("arguments error: start <= stop for negative step")
+
+    current = start
+    while (step > 0 and current < stop) or (step < 0 and current > stop):
+        yield current
+        current += step
+
+
+for i in my_range(10, 100, 0):
+    print(i)
