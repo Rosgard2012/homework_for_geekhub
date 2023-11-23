d5cea4319ee795726e18a9c53c6b4ab6b80e7f11
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-11-23 08:14:13 +0200

    task_02

--- HT_11/task_02.py
+++ HT_11/task_02.py
@@ -4,3 +4,36 @@
 show_all_information.
 - Створіть 2 екземпляри класу Person та в кожному з екземплярів створіть
  атребут profession (його не має інсувати під час ініціалізації)."""
+
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def show_age(self):
+        return f"{self.name} is {self.age} years old."
+
+    def print_name(self):
+        return f"The name is {self.name}."
+
+    def show_all_information(self):
+        return f"Name: {self.name}, Age: {self.age}"
+
+
+person1 = Person("Alisa", 30)
+person2 = Person("Borisa", 25)
+
+
+person1.profession = "PM"
+person2.profession = "QA"
+
+
+print(person1.show_age())
+print(person2.show_age())
+print(person1.print_name())
+print(person2.print_name())
+print(person1.show_all_information())
+print(person2.show_all_information())
+print(person1.profession)
+print(person2.profession)
