bbcce4e4b4ff6a3e958d7020cb4c24b20e841ea6
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-11-30 07:45:58 +0200

    HT_13 task_03

--- HT_13/task_03.py
+++ HT_13/task_03.py
@@ -1,2 +1,18 @@
 '''3. Створіть клас в якому буде атребут який буде рахувати кількість
  створених екземплярів класів.'''
+
+
+class MyClass:
+    count = 0
+    def __init__(self):
+        MyClass.count += 1
+
+
+obj1 = MyClass()
+print(obj1.count)
+
+obj2 = MyClass()
+print(obj2.count)
+
+obj3 = MyClass()
+print(obj3.count)
