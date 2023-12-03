c913ca60c13696090aedf1c483a564c205fc8c3e
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-12-03 23:07:58 +0200

    HT_14  task 3 start

---
+++ HT_14/task_03/task_03.py
@@ -0,0 +1,29 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+# Створюємо CSV-файл для збереження даних
+csv_file = open('quotes.csv', 'w', newline='', encoding='utf-8')
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['Quote', 'Author', 'Tags'])
+
+# Збираємо дані з 10 сторінок
+for page_num in range(1, 11):
+    url = f'http://quotes.toscrape.com/page/{page_num}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        quotes = soup.find_all(class_='quote')
+
+        for quote in quotes:
+            quote_text = quote.find(class_='text').get_text(strip=True)
+            author = quote.find(class_='author').get_text(strip=True)
+            tags = ', '.join(tag.get_text(strip=True) for tag in quote.find_all(class_='tag'))
+
+            csv_writer.writerow([quote_text, author, tags])
+
+    else:
+        print(f'Failed to fetch page {page_num}')
+
+csv_file.close()
