f9bd0583bdccc1c1922691b7b1d824c90d11f602
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-12-04 00:19:51 +0200

    HT_14  task 3 in progres 2

--- HT_14/task_03/task_03.py
+++ HT_14/task_03/task_03.py
@@ -6,7 +6,7 @@ import csv
 def scrape_quotes():
     csv_file = open('quotes.csv', 'w', newline='', encoding='utf-8')
     csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Quote', 'Author', 'Tags'])
+    csv_writer.writerow(['Quote', 'Author', 'Tags', 'Born', 'Location', 'Description'])
 
     for page_num in range(1, 11):
         url = f'http://quotes.toscrape.com/page/{page_num}'
@@ -25,38 +25,66 @@ def scrape_quotes():
 
         else:
             print(f'Failed to fetch page {page_num}')
-
-    csv_file.close()
-
-def scrape_authors():
-    csv_file = open('authors.csv', 'w', newline='', encoding='utf-8')
-    csv_writer = csv.writer(csv_file)
-    csv_writer.writerow(['Author', 'Born', 'Location', 'Description'])
-
-    author_urls = [
-        'http://quotes.toscrape.com/author/J-K-Rowling',
-        # Додайте інші URL-адреси авторів, якщо потрібно
-    ]
-
-    for url in author_urls:
-        response = requests.get(url)
-
-        if response.status_code == 200:
-            soup = BeautifulSoup(response.content, 'html.parser')
-            author_name = soup.find(class_='author-title').get_text(strip=True)
-            born_date = soup.find(class_='author-born-date').get_text(strip=True)
-            born_location = soup.find(class_='author-born-location').get_text(strip=True)
-            description = soup.find(class_='author-description').get_text(strip=True)
-
-            csv_writer.writerow([author_name, born_date, born_location, description])
-
-        else:
-            print(f'Failed to fetch author page {url}')
-
+        author_url = quote.find('a', href=True)['href']
+        author_details = scrape_author_details('http://quotes.toscrape.com' + author_url)
+        csv_writer.writerow([quote_text, author, tags,
+                             author_details['Born'],
+                             author_details['Location'],
+                             author_details['Description']])
     csv_file.close()
 
 
+def scrape_author_details(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        author_name = soup.find(class_='author-title').get_text(strip=True)
+        born_date = soup.find(class_='author-born-date').get_text(strip=True)
+        born_location = soup.find(class_='author-born-location').get_text(strip=True)
+        description = soup.find(class_='author-description').get_text(strip=True)
+
+        return {
+            'Author': author_name,
+            'Born': born_date,
+            'Location': born_location,
+            'Description': description
+        }
+    else:
+        print(f'Failed to fetch author details from {url}')
+        return None
 
 
-scrape_authors()
 scrape_quotes()
+
+# def scrape_authors():
+#     csv_file = open('quotes.csv', 'w', newline='', encoding='utf-8')
+#     csv_writer = csv.writer(csv_file)
+#     csv_writer.writerow(['Quote', 'Author', 'Tags', 'Born', 'Location', 'Description'])
+#
+#     author_urls = [
+#         'http://quotes.toscrape.com/author/J-K-Rowling',
+#         # Додайте інші URL-адреси авторів, якщо потрібно
+#     ]
+#
+#     for url in author_urls:
+#         response = requests.get(url)
+#
+#         if response.status_code == 200:
+#             soup = BeautifulSoup(response.content, 'html.parser')
+#             author_name = soup.find(class_='author-title').get_text(strip=True)
+#             born_date = soup.find(class_='author-born-date').get_text(strip=True)
+#             born_location = soup.find(class_='author-born-location').get_text(strip=True)
+#             description = soup.find(class_='author-description').get_text(strip=True)
+#
+#             csv_writer.writerow([author_name, born_date, born_location, description])
+#
+#         else:
+#             print(f'Failed to fetch author page {url}')
+#
+#     csv_file.close()
+
+
+
+
+# scrape_authors()
+
