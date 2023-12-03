4e6a602b6c148025d844b52d30f7efd89d53470d
Pavlo Vykhrov <vihrow.pavlo@gmail.com>
2023-12-03 20:37:32 +0200

    HT_13 fix task 4

--- HT_13/task_04.py
+++ HT_13/task_04.py
@@ -8,15 +8,29 @@ class CustomList:
         self._data = list(args)
 
     def __getitem__(self, index):
-        if index < 1:
-            raise IndexError("Index must be greater than 0")
+        if index == 0:
+            raise IndexError("Index starts from 1, not 0")
+        elif index < 0:
+            return self._data[index]
         return self._data[index - 1]
+
+    def __setitem__(self, index, value):
+        if index == 0:
+            raise IndexError("Index starts from 1, not 0")
+        else:
+            self._data[index - 1] = value
 
     def __len__(self):
         return len(self._data)
 
     def __repr__(self):
         return repr(self._data)
+
+    def __delitem__(self, index):
+        if index == 0:
+            raise IndexError("Index starts from 1, not 0")
+        else:
+            del self._data[index - 1]
 
     def append(self, value):
         self._data.append(value)
@@ -33,4 +47,20 @@ custom.append(40)
 print(custom)
 print(len(custom))
 print(custom.pop())
+custom.append(50)
 print(custom)
+custom.__delitem__(3)
+print(custom)
+custom[3] = 100
+print(custom)
+
+
+"""
+[10, 20, 30]
+10
+3
+[10, 20, 30, 40]
+4
+40
+[10, 20, 30]
+"""
